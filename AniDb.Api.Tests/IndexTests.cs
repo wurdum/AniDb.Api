9b4ac5dd88883442f5c9e1bd22efde69c8437dba
wurdum <pavel.ritikov@gmail.com>
2013-11-14 19:10:54 +0200

    repo init

---
+++ AniDb.Api.Tests/IndexTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace AniDb.Api.Tests
+{
+    [TestFixture]
+    public class IndexTests
+    {
+        [Test]
+        public void IndexIsNeverEmpty() {
+            var index = new Index();
+
+            Assert.True(index.Count > 0);
+            Assert.AreEqual(index.Find("❄").Value.Id, 4726);
+        }
+    }
+}
---
+++ AniDb.Api/Index.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Kent.Boogaart.KBCsv;
+
+namespace AniDb.Api
+{
+    public class Index
+    {
+        public const string DataDir = "Data";
+        public const string IndexFileName = "anime-titles.dat";
+        private static Entry[] _entries;
+
+        static Index() {
+            var programmRoot = Path.GetDirectoryName(new Uri(Assembly.GetExecutingAssembly().CodeBase).LocalPath);
+            var indexFile = Path.Combine(programmRoot, DataDir, IndexFileName);   
+
+            var entries = new List<Entry>(44000);
+            using (var reader = new CsvReader(File.OpenRead(indexFile), Encoding.UTF8) { ValueSeparator = '|' }) {
+                
+                reader.SkipRecords(3);
+                DataRecord record;
+                while ((record = reader.ReadDataRecord()) != null)
+                    entries.Add(new Entry(Convert.ToInt32(record[0]), (EntryType)Convert.ToInt32(record[1]), record[2], record[3]));
+            }
+
+            _entries = entries.ToArray();
+        }
+
+        public int Count {
+            get { return _entries.Length; }
+        }
+
+        public Entry? Find(string title) {
+            var entry = _entries.FirstOrDefault(e => e.Title == title);
+            if (entry.Equals(default(Entry)))
+                return null;
+
+            return entry;
+        }
+
+        public enum EntryType { PrimaryTitle = 1, Synonyms, ShortTitles, OfficialTitle }
+        
+        public struct Entry
+        {
+            public readonly int Id;
+            public readonly EntryType Type;
+            public readonly string Language;
+            public readonly string Title;
+
+            public Entry(int id, EntryType type, string language, string title) {
+                Id = id;
+                Type = type;
+                Language = language;
+                Title = title;
+            }
+        }
+    }
+}
